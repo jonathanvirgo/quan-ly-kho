6885911a4a7c56d39dd090b43256ada5be82eaa9
Đạt <qdatvirgo@gmail.com>
2019-08-15 11:39:55 +0700

    mobile

--- src/app/he-thong/layout/layout.module.ts
+++ src/app/he-thong/layout/layout.module.ts
@@ -5,9 +5,10 @@ import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
 import { AppSettingComponent } from './app-setting/app-setting.component';
 import {RouterModule} from '@angular/router';
+import { NavTopComponent } from './nav-top/nav-top.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, NavComponent, AppSettingComponent],
+  declarations: [HeaderComponent, FooterComponent, NavComponent, AppSettingComponent, NavTopComponent],
   imports: [
     CommonModule,
     RouterModule
